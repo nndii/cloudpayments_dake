6091f87e762d688cf0bbd4542c8ba00ab0131865
nndii <midnightcowboy@rape.lol>
2018-03-06 17:45:00 +0800

    confirm view tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,22 @@
 import pytest
+import datetime
 
 from src import create_app
+from src.resources import Transaction
+
+
+test_transaction = Transaction(
+    transaction_id=777,
+    amount=54.7,
+    datetime=datetime.datetime.utcnow(),
+    name='test',
+    ip_address='test',
+    card_cryptogram_packet='test'
+)
 
 
 @pytest.fixture
 def cli(loop, aiohttp_client):
     app = create_app()
+    app['TRANSACTION_DB'][777] = test_transaction
     return loop.run_until_complete(aiohttp_client(app))
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -59,3 +59,37 @@ async def test_auth__not_enough_params(aiohttp_server, cli):
     assert response.status == 200
     assert response_json['Success'] is False
     assert response_json['Message'] == 'Not enough parameters to proceed'
+
+
+# =================== CONFIRM ==========================
+
+
+async def test_confirm__success(cli, aiohttp_server):
+    app = web.Application()
+    app.router.add_post('/', dummy_response__code_0)
+    server = await aiohttp_server(app, port=8080)
+
+    params = {
+        'Amount': 54.7,
+        'TransactionId': 777
+    }
+    response = await cli.post('/payments/confirm/', json=params)
+    response_json = await response.json()
+    assert response.status == 200
+    assert response_json['Success'] is True
+
+
+async def test_confirm__not_found(cli, aiohttp_server):
+    app = web.Application()
+    app.router.add_post('/', dummy_response__code_0)
+    server = await aiohttp_server(app, port=8080)
+
+    params = {
+        'Amount': 123.2,
+        'TransactionId': 33
+    }
+    response = await cli.post('/payments/confirm/', json=params)
+    response_json = await response.json()
+    assert response.status == 200
+    assert response_json['Success'] is False
+    assert response_json['Message'] == 'Transaction not found'
